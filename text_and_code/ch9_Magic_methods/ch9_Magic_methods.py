41c5e1a5d22e4a113ab2d0e880485924975c1f70
WaylonWong <69142725+WaylonWong@users.noreply.github.com>
2025-04-21 20:47:07 +0100

    add Eight queens problem in file: Eight queens problemtext_and_code\ch9_Magic_methods\ch9_Magic_methods.py

--- text_and_code/ch9_Magic_methods/ch9_Magic_methods.py
+++ text_and_code/ch9_Magic_methods/ch9_Magic_methods.py
@@ -131,3 +131,17 @@ def recur_flatten(nested):
                 yield element
     except TypeError:
         yield nested
+        
+# Eight queens problem
+def conflict(state, nextX):
+    nextY = len(state)
+    for i in range(nextY):
+        if abs(state[i] - nextX) in (0, nextY - i):
+        return True
+    return False
+
+def queens(num, state):
+    if len(state) == num-1:
+        for pos in range(num):
+            if not conflict(state, pos):
+                yield pos
