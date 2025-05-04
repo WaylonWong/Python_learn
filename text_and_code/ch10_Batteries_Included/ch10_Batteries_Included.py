6aba04665133179662a7270134957ed2c2056474
WaylonWong <69142725+WaylonWong@users.noreply.github.com>
2025-05-04 17:45:54 +0100

    add some new lines and two new files

--- text_and_code/ch10_Batteries_Included/ch10_Batteries_Included.py
+++ text_and_code/ch10_Batteries_Included/ch10_Batteries_Included.py
@@ -5,3 +5,12 @@ def hello():
 def test():
     hello()
 if __name__ == '__main__': test()
+
+
+# Adding Line Numbers to a Python Script
+# numberlines.py
+import fileinput
+for line in fileinput.input(inplace=True):
+    line = line.rstrip()
+    num = fileinput.lineno()
+    print('{:<50} # {:2d}'.format(line, num))
